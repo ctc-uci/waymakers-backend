e893cc01da78d19331a9dad46d98b2e018ae40e0
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-11-16 22:03:32 -0800

    deleted extraneous sql files

--- postgres/accounts.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE accounts_database;
-
-CREATE TABLE accounts(
-    account_id SERIAL PRIMARY KEY,
-    username varchar(255),
-    password varchar(255)
-);
--- postgres/inventory.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE inventory_database;
-
-CREATE TABLE items(
-    id SERIAL PRIMARY KEY,
-    name varchar(255),
-    in_stock INTEGER,
-    number_needed INTEGER,
-    category varchar(255)
-);

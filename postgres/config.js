385c477ba3eddc92d63659cb5b2e1d53d68b6dae
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-11-18 14:47:19 -0800

    removed extraneous config pools

--- postgres/config.js
+++ postgres/config.js
@@ -1,25 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config({ path: `${__dirname}/.env` });
 
-/**
-const pool = new Pool({
-  user: "wmk_dev",
-  host: "waymakers-dev-db.c3g5iuul9ddg.us-west-1.rds.amazonaws.com",
-  database: "postgres",
-  password: "CHANG1NGtheWoRld2021",
-  port: 5432
-});
-*/
-/**
-const pool = new Pool({
-  user: 'postgres',
-  password: 'ctc123',
-  host: 'localhost',
-  port: 5432,
-  database: 'inventory_database',
-});
-*/
-
 const pool = new Pool({
   user: process.env.REACT_APP_AWS_USER,
   host: process.env.REACT_APP_AWS_HOST,
@@ -28,7 +9,6 @@ const pool = new Pool({
   port: process.env.REACT_APP_AWS_PORT,
 });
 
-console.log(process.env.REACT_APP_AWS_USER);
 module.exports = pool;
 
 // Example testing to connect to AWS

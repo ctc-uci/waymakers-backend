da85452a0ba1c5531825f12471891070dcadc26c
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-12-03 19:47:10 -0800

    fixed some syntax

--- routes/inventory/inventory.js
+++ routes/inventory/inventory.js
@@ -16,34 +16,37 @@ inventoryRouter.get('/', async (req, res) => {
   }
 });
 
-// Get an item by category
-inventoryRouter.get('/category/:category', async (req, res) => {
-  const { category } = req.params;
+// Get an item by WAREHOUSE
+inventoryRouter.get('search/', async (req, res) => {
+  const { warehouse, category, substring } = req.params;
+  let items;
   try {
-    const item = await pool.query(`SELECT * FROM items WHERE category = '${category}'`);
-    res.send(item.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// SEARCH for an item with SUBSTRING
-inventoryRouter.get('/search/:substring', async (req, res) => {
-  try {
-    const { substring } = req.params;
-    const item = await pool.query(`SELECT * FROM items WHERE strpos(name, '${substring}') > 0`);
-    res.send(item.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// SEARCH for an item with SUBSTRING and CATEGORY
-inventoryRouter.get('/search/:substring/:category', async (req, res) => {
-  try {
-    const { category, substring } = req.params;
-    const item = await pool.query(`SELECT * FROM items WHERE strpos(name, '${substring}') > 0 AND category = '${category}'`);
-    res.send(item.rows);
+    // GETTING EVERY ITEM!
+    if (substring === '' && category === '' && warehouse === '') {
+      items = await pool.query('SELECT * FROM items');
+      // Searching with SUBSTRING
+    } else if (warehouse === '' && category === '') {
+      items = await pool.query(`SELECT * FROM items WHERE strpos(name, '${substring}') > 0`);
+      // Searching with WAREHOUSE
+    } else if (category === '' && substring === '') {
+      items = await pool.query('SELECT * FROM items WHERE warehouse = warehouse');
+      // Searching with CATEGORY
+    } else if (warehouse === '' && substring === '') {
+      items = await pool.query(`SELECT * FROM items WHERE category = '${category}`);
+      // Searching with CATEGORY and SUBSTRING
+    } else if (warehouse === '') {
+      items = await pool.query(`SELECT * FROM items WHERE category = '${category}' AND strpos(name, '${substring}') > 0`);
+      // Searching with WAREHOUSE and SUBSTRING
+    } else if (category === '') {
+      items = await pool.query(`SELECT * FROM items WHERE warehouse = '${warehouse}' AND strpos(name, '${substring}') > 0`);
+      // Searching with WAREHOUSE and CATEGORY
+    } else if (substring === '') {
+      items = await pool.query(`SELECT * FROM items WHERE warehouse = '${warehouse}' AND category = '${category}' > 0`);
+      // Searching with WAREHOUSE and CATEGORY and SUBSTRING
+    } else {
+      items = await pool.query(`SELECT * FROM items WHERE warehouse = '${warehouse}' AND category = '${category}' > 0 AND strpos(name, '${substring}') > 0`);
+    }
+    res.send(items.rows);
   } catch (err) {
     console.error(err.message);
   }
@@ -53,9 +56,9 @@ inventoryRouter.get('/search/:substring/:category', async (req, res) => {
 inventoryRouter.post('/', async (req, res) => {
   try {
     const {
-      name, quantity, needed, category,
+      name, quantity, needed, category, warehouse,
     } = req.body;
-    const newItem = await pool.query(`INSERT INTO items (name, quantity, needed, category) VALUES ('${name}', '${quantity}', '${needed}' , '${category}') RETURNING *`);
+    const newItem = await pool.query(`INSERT INTO items (name, quantity, needed, category, warehouse) VALUES ('${name}', '${quantity}', '${needed}' , '${category}', '${warehouse}') RETURNING *`);
     res.send(newItem.rows);
   } catch (err) {
     console.error(err.message);
@@ -67,12 +70,13 @@ inventoryRouter.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const {
-      name, quantity, needed, category,
+      name, quantity, needed, category, warehouse,
     } = req.body;
     if (name) await pool.query(`UPDATE items SET name = '${name}' WHERE id = ${id}`);
     if (quantity) await pool.query(`UPDATE items SET quantity = '${quantity}' WHERE id = ${id}`);
     if (needed) await pool.query(`UPDATE items SET needed = '${needed}' WHERE id = ${id}`);
     if (category) await pool.query(`UPDATE items SET category = '${category}' WHERE id = ${id}`);
+    if (warehouse) await pool.query(`UPDATE items SET warehouse = '${warehouse}' WHERE id = ${id}`);
     res.send(`Item with id ${id} was updated!`);
   } catch (err) {
     console.error(err.message);

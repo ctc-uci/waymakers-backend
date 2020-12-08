9d8da9ad8a5c13dccfc732d34c672728c2e4b9be
Alan Chang <alan@alanchang.xyz>
2020-12-08 06:49:04 +0000

    Resolve "REF: Create Events Components and Functionality"

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 // const db = require("./postgres/config.js");
 
 // routes
 const accountRouter = require('./routes/accounts/accounts');
+const eventRouter = require('./routes/events/events');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -16,6 +19,7 @@ app.use(
 );
 
 app.use('/accounts', accountRouter);
+app.use('/events', eventRouter);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);

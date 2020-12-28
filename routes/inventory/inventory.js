c59c3bce47b8f0319c57fa85501d4bc03c9d5d1e
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2020-12-27 17:14:38 -0800

    changed backend api to support new tables

--- routes/inventory/inventory.js
+++ routes/inventory/inventory.js
@@ -19,14 +19,12 @@ inventoryRouter.get('/', async (req, res) => {
 // Get an item by WAREHOUSE
 inventoryRouter.get('/get/', async (req, res) => {
   const {
-    division, warehouse, category, search,
+    division, category, search,
   } = req.query;
-  console.log(division, warehouse, category, search);
   try {
-    const items = await pool.query(`SELECT * FROM items WHERE (warehouse_id = (SELECT id FROM warehouses WHERE warehouselabel='${warehouse}') OR '${warehouse}' = '') AND 
-    (category_id = (SELECT id FROM categories WHERE label='${category}') OR '${category}' = '') AND 
-    (strpos(name, '${search}') > 0 OR '${search}' = '') AND 
-    (div_num=(SELECT div_num FROM divisions WHERE div_name='${division}') OR '${division}'='undefined')`);
+    const items = await pool.query(`SELECT * FROM items WHERE (div_num = (SELECT div_num FROM divisions WHERE div_name='${division}') OR '${division}' = '') AND 
+    (category_id = (SELECT id FROM item_categories WHERE label='${category}') OR '${category}' = '') AND 
+    (LOWER(name) LIKE LOWER('%${search}%') OR '${search}' = '')`);
     res.send(items.rows);
   } catch (err) {
     console.error(err.message);
@@ -37,9 +35,10 @@ inventoryRouter.get('/get/', async (req, res) => {
 inventoryRouter.post('/', async (req, res) => {
   try {
     const {
-      name, quantity, needed, division, warehouse, category,
+      name, quantity, needed, division, category,
     } = req.body;
-    const newItem = await pool.query(`INSERT INTO items (name, quantity, needed, div_num, warehouse_id, category_id) VALUES ('${name}', '${quantity}', '${needed}' , (SELECT div_num from divisions WHERE div_name='${division}'), (SELECT id from categories WHERE label='${category}'), (SELECT id from warehouses WHERE warehouselabel='${warehouse}')) RETURNING *`);
+    console.log(name, quantity, needed, division, category);
+    const newItem = await pool.query(`INSERT INTO items (name, quantity, needed, div_num, category_id) VALUES ('${name}', '${quantity}', '${needed}' , (SELECT div_num from divisions WHERE div_name='${division}'), (SELECT id from item_categories WHERE label='${category}'))`);
     res.send(newItem.rows);
   } catch (err) {
     console.error(err.message);
@@ -56,7 +55,7 @@ inventoryRouter.put('/:id', async (req, res) => {
     if (name) await pool.query(`UPDATE items SET name = '${name}' WHERE id = ${id}`);
     if (quantity) await pool.query(`UPDATE items SET quantity = '${quantity}' WHERE id = ${id}`);
     if (needed) await pool.query(`UPDATE items SET needed = '${needed}' WHERE id = ${id}`);
-    if (category) await pool.query(`UPDATE items SET category = '${category}' WHERE id = ${id}`);
+    if (category) await pool.query(`UPDATE items SET category_id = (SELECT id from item_categories WHERE label='${category}') WHERE id = ${id}`);
     res.send(`Item with id ${id} was updated!`);
   } catch (err) {
     console.error(err.message);

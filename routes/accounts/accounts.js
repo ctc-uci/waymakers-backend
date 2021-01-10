832449480095b497f48f8667abafcb116dfa597a
Rashmi Sharma <rashmisharma489@gmail.com>
2021-01-10 09:40:55 +0000

    Resolve "REF: Create user Profile Page and Functionality"

--- routes/accounts/accounts.js
+++ routes/accounts/accounts.js
@@ -9,32 +9,53 @@ accountRouter.use(express.json());
 // Get all accounts
 accountRouter.get('/', async (req, res) => {
   try {
-    const allAccounts = await pool.query('SELECT * FROM accounts');
+    const allAccounts = await pool.query('SELECT * FROM users');
     res.send(allAccounts.rows);
   } catch (err) {
-    console.error(err.message);
+    res.status(400).send(err.message);
   }
 });
 
 // Get an account
 accountRouter.get('/:id', async (req, res) => {
+  console.log('request made to /:id route');
+
   const { id } = req.params;
   try {
-    const account = await pool.query(`SELECT * FROM accounts WHERE account_id = ${id}`);
-    res.send(account.rows);
+    const account = await pool.query(`SELECT * FROM users WHERE userid = '${id}'`);
+    const permission = await pool.query(`SELECT * FROM permissions WHERE userid = '${id}'`);
+    const availability = await pool.query(`SELECT * FROM availability WHERE userid = '${id}'`);
+    res.send({
+      account: account.rows[0],
+      permissions: permission.rows[0],
+      availability: availability.rows,
+    });
   } catch (err) {
-    console.error(err.message);
+    res.status(400).send(err.message);
   }
 });
 
-// Create an accountnp
+// Create an account
 accountRouter.post('/', async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const newAccount = await pool.query(`INSERT INTO accounts (username, password) VALUES ('${username}', '${password}') RETURNING *`);
-    res.send(newAccount.rows);
+    const {
+      userID, firstName, lastName, birthDate, locationStreet, locationCity,
+      locationState, locationZip, tier, permission,
+    } = req.body;
+    const newAccount = await pool.query(`
+      INSERT INTO users (userid, firstname, lastname, birthdate, locationstreet, locationcity,
+      locationstate, locationzip, tier)
+      VALUES ('${userID}', '${firstName}', '${lastName}', '${birthDate}', '${locationStreet}',
+      '${locationCity}', '${locationState}', '${locationZip}', '${tier}') RETURNING *`);
+    const newPermission = await pool.query(`
+      INSERT INTO permissions (userid, permissions) 
+      VALUES ('${userID}', '${permission}') RETURNING *`);
+    res.send({
+      newAccount: newAccount.rows[0],
+      newPermission: newPermission.rows[0],
+    });
   } catch (err) {
-    console.error(err.message);
+    res.status(400).send(err.message);
   }
 });
 
@@ -42,12 +63,30 @@ accountRouter.post('/', async (req, res) => {
 accountRouter.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const { username, password } = req.body;
-    if (username) await pool.query(`UPDATE accounts SET username = '${username}' WHERE account_id = ${id}`);
-    if (password) await pool.query(`UPDATE accounts SET password = '${password}' WHERE account_id = ${id}`);
+    const {
+      firstName, lastName, birthDate, locationStreet, locationCity,
+      locationState, locationZip, tier, permission,
+    } = req.body;
+
+    const userQuery = `
+                UPDATE users
+                  SET ${firstName ? `firstname = '${firstName}',` : ''}
+                      ${lastName ? `lastname = '${lastName}',` : ''}
+                      ${birthDate ? `birthdate = '${birthDate}',` : ''}
+                      ${locationStreet ? `locationstreet = '${locationStreet}',` : ''}
+                      ${locationCity ? `locationcity = '${locationCity}',` : ''}
+                      ${locationState ? `locationstate = '${locationState}',` : ''}
+                      ${locationZip ? `locationzip = '${locationZip}',` : ''}
+                      ${tier ? `tier = '${tier}'` : ''}
+                  WHERE userid = '${id}'
+                `;
+    await pool.query(userQuery);
+
+    if (permission) await pool.query(`UPDATE permissions SET permissions = '${permission}' WHERE userid = '${id}'`);
+
     res.send(`Account with id ${id} was updated!`);
   } catch (err) {
-    console.error(err.message);
+    res.status(400).send(err.message);
   }
 });
 
@@ -55,10 +94,10 @@ accountRouter.put('/:id', async (req, res) => {
 accountRouter.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    await pool.query(`DELETE FROM accounts WHERE account_id = ${id}`);
+    await pool.query(`DELETE FROM users WHERE userid = '${id}'`);
     res.send(`Account with id ${id} was deleted.`);
   } catch (err) {
-    console.error(err.message);
+    res.status(400).send(err.message);
   }
 });
 

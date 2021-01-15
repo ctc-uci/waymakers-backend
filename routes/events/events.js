5639c3a28942941b6e1472878167e6381eb99834
Rostam Vakhshoori <rvakhsho@uci.edu>
2021-01-14 19:17:33 -0800

    added error checks

--- routes/events/events.js
+++ routes/events/events.js
@@ -21,16 +21,12 @@ function convertEventSnakeToCamel(events) {
 // Get all events
 eventRouter.get('/', async (req, res) => {
   try {
-    // await pool.query(`SET TIMEZONE = 'America/Los_Angeles';`);
     let allEvents = await pool.query('SELECT * FROM events;');
-    // let i = new Date(allEvents.rows[0].start_time);
-    // i = i.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-    // console.log(i);
-    // console.log("HELLO");
     allEvents = convertEventSnakeToCamel(allEvents.rows);
-    res.send(allEvents);
+    res.status(200).send(allEvents);
   } catch (err) {
     console.error(err.message);
+    res.status(500).send(err.message);
   }
 });
 
@@ -40,9 +36,14 @@ eventRouter.get('/:id', async (req, res) => {
   try {
     let event = await pool.query(`SELECT * FROM events WHERE event_id = ${id}`);
     event = convertEventSnakeToCamel(event.rows);
-    res.send(event);
+    if (event.length === 0) {
+      res.status(400).send(event);
+    } else {
+      res.status(200).send(event);
+    }
   } catch (err) {
     console.error(err.message);
+    res.status(500).send(err.message);
   }
 });
 
@@ -54,13 +55,18 @@ eventRouter.post('/add', async (req, res) => {
       eventName, eventLocation, eventDescription, startTime, endTime, isAllDay, division,
     } = req.body;
 
-    await pool.query(`INSERT INTO events 
+    const response = await pool.query(`INSERT INTO events 
                       (event_name, event_location, event_description, start_time, end_time, all_day, division) 
-                      VALUES ('${eventName}', '${eventLocation}', '${eventDescription}', '${startTime}', '${endTime}', '${isAllDay}', '${division}')`);
-    res.send(`${eventName} added!`);
+                      VALUES ('${eventName}', '${eventLocation}', '${eventDescription}', '${startTime}', '${endTime}', '${isAllDay}', '${division}')
+                      RETURNING *`);
+    if (response.rowCount === 0) {
+      res.status(400).send(response);
+    } else {
+      res.status(200).send(response);
+    }
   } catch (err) {
-    res.send(err.message);
     console.error(err.message);
+    res.status(500).send(err.message);
   }
 });
 
@@ -73,7 +79,7 @@ eventRouter.put('/:id', async (req, res) => {
     const {
       eventName, eventLocation, eventDescription, startTime, endTime, isAllDay, division,
     } = req.body;
-    await pool.query(`UPDATE events 
+    const response = await pool.query(`UPDATE events 
                       SET event_name = '${eventName}', 
                       event_location = '${eventLocation}', 
                       event_description = '${eventDescription}',
@@ -81,10 +87,16 @@ eventRouter.put('/:id', async (req, res) => {
                       end_time = '${endTime}', 
                       all_day = '${isAllDay}',
                       division = '${division}' 
-                      WHERE event_id = ${id}`);
-    res.send(`event with id ${id} was updated!`);
+                      WHERE event_id = ${id}
+                      RETURNING *`);
+    if (response.rowCount === 0) {
+      res.status(400).send();
+    } else {
+      res.status(200).send(response);
+    }
   } catch (err) {
     console.error(err.message);
+    res.status(500).send(err.message);
   }
 });
 
@@ -92,10 +104,15 @@ eventRouter.put('/:id', async (req, res) => {
 eventRouter.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    await pool.query(`DELETE FROM events WHERE event_id = ${id}`);
-    res.send(`Event with id ${id} was deleted.`);
+    const response = await pool.query(`DELETE FROM events WHERE event_id = ${id} RETURNING *`);
+    if (response.rowCount === 0) {
+      res.status(400).send();
+    } else {
+      res.status(200).send(response);
+    }
   } catch (err) {
     console.error(err.message);
+    res.status(500).send(err.message);
   }
 });
 

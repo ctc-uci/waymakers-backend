8a64c8c8efcd8654caff25ead497ec6f1e2874a6
Leon Lin <leonlin.48@gmail.com>
2021-01-22 09:16:51 +0000

    Create Access Tokens

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { authRouter, verifyToken } = require('./routes/auth/auth');
 // const db = require("./postgres/config.js");
+
+require('dotenv').config();
 
 // routes
 const accountRouter = require('./routes/accounts/accounts');
 const eventRouter = require('./routes/events/events');
 
 const app = express();
+
 const port = 3001;
 
+const reactAppHost = process.env.WMK_REACT_APP_HOST;
+const reactAppPort = process.env.WMK_REACT_APP_PORT
+
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cookieParser());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   }),
 );
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: `${reactAppHost}:${reactAppPort}`
+}));
 
-app.use('/accounts', accountRouter);
-app.use('/events', eventRouter);
+app.use('/accounts', [verifyToken, accountRouter]);
+app.use('/events', [verifyToken, eventRouter]);
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at ${reactAppHost}:${port}`);
 });

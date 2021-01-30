1bb373a27c40e0a661aad994af5d6572fa591100
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2021-01-29 16:21:33 -0800

    Fixed volunteerData get all

--- routes/events/volunteerData.js
+++ routes/events/volunteerData.js
@@ -9,7 +9,6 @@ volunteerDataRouter.use(express.json());
 // Get Top 4 Volunteers
 volunteerDataRouter.get('/top/', async (req, res) => {
   const { event } = req.query;
-  // console.log(event);
   try {
     const allEvents = await pool.query(`SELECT log_hours.userid, firstname, lastname, SUM(total_hours) FROM log_hours
                                         INNER JOIN users ON
@@ -34,13 +33,17 @@ volunteerDataRouter.get('/top/', async (req, res) => {
 // Get all volunteers
 volunteerDataRouter.get('/all/', async (req, res) => {
   const { event } = req.query;
-  console.log(event);
   try {
-    const allEvents = await pool.query(`SELECT users.firstname, users.lastname, users.userid
-                                            FROM user_event
-                                            INNER JOIN users ON
-                                            user_event.userid=users.userid
-                                            WHERE user_event.event_id=${event};`);
+    const allEvents = await pool.query(`SELECT USERS.FIRSTNAME,
+                                          USERS.LASTNAME,
+                                          USERS.USERID,
+                                          SUM(TOTAL_HOURS)
+                                        FROM USER_EVENT
+                                        INNER JOIN USERS ON USER_EVENT.USERID = USERS.USERID
+                                        INNER JOIN LOG_HOURS ON LOG_HOURS.EVENT_ID = USER_EVENT.EVENT_ID
+                                        WHERE USER_EVENT.EVENT_ID = $1
+                                        GROUP BY USERS.USERID`,
+    [event]);
     res.status(200).send(allEvents.rows);
   } catch (err) {
     console.error(err.message);

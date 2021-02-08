85149bb3294ff1f24394a7730dcfd0947494872c
Becky Dinh <beckyd@uci.edu>
2021-02-07 16:27:52 -0800

    moved qualification and qualification_list into accounts.sql

--- postgres/sql/accounts.sql
+++ postgres/sql/accounts.sql
@@ -27,3 +27,25 @@ CREATE TABLE Availability
     endtime TIME NOT NULL,
     FOREIGN KEY (userid) REFERENCES Users (userid) ON DELETE CASCADE
 );
+
+CREATE TABLE public.qualification_list
+(
+    id integer NOT NULL,
+    volunteer_tier integer,
+    create_timestamp timestamp without time zone,
+    CONSTRAINT qualification_list_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.qualification
+(
+    id integer NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    question character varying COLLATE pg_catalog."default",
+    qualification_list_id integer,
+    CONSTRAINT qualification_pkey PRIMARY KEY (id),
+    CONSTRAINT qualification_list_id FOREIGN KEY (qualification_list_id)
+        REFERENCES public.qualification_list (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+

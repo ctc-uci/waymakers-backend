a74bbeddfc9961e1c9f13c4b720db9c3af192a45
Becky Dinh <beckyd@uci.edu>
2021-02-09 23:26:52 -0800

    correct syntax for routes

--- routes/accounts/qualifications.js
+++ routes/accounts/qualifications.js
@@ -8,55 +8,46 @@ qualificationsRouter.use(express.json());
 
 // Get qualification list by id
 qualificationsRouter.get('/', async (req, res) => {
-const { id } = req.params;
-try {
-    const qualification = await pool.query(`SELECT * FROM qualification_list WHERE id = $1`, [id]);
+  const { id } = req.body;
+  try {
+    const qualification = await pool.query('SELECT * FROM qualification_list WHERE id = $1', [id]);
     res.send({
-        qualification: qualification.rows
-    })
-    
-} catch(err) {
+      qualification: qualification.rows,
+    });
+  } catch (err) {
     res.status(400).send(err.message);
-}
+  }
 });
 
 // Create qualification list
 qualificationsRouter.post('/', async (req, res) => {
-    try {
-      const {
-        volunteerTier
-      } = req.body;
-      const newQualificationList = await pool.query(`
+  try {
+    const {
+      volunteerTier,
+    } = req.body;
+    const newQualificationList = await pool.query(`
         INSERT INTO qualification_list(volunteer_tier, create_timestamp) VALUES
         ($1,
-        (SELECT NOW()::timestamp)) RETURNING *`
-        [volunteerTier]);
-      res.send({
-        volunteerTier: newQualificationList.rows,
-      });
-    } catch (err) {
-      res.status(400).send(err.message);
-    }
-  });
-
-// Edit qualification list
-qualificationsRouter.put('/', async(req, res) => {
-    try {
-        
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+        (SELECT NOW()::timestamp)) RETURNING *`,
+    [volunteerTier]);
+    res.send({
+      volunteerTier: newQualificationList.rows,
+    });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
 
 // Delete qualification list
-qualificationsRouter.delete('/', async(req, res) => {
-    try {
-        const { id } = req.params;
-        await pool.query('DELETE FROM item WHERE id = $1', [id]);
-        res.send(`Qualification with id ${id} was deleted!`);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+qualificationsRouter.delete('/', async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (id == null) res.status(400).send("Can't delete qualification_list without ID");
+    await pool.query('DELETE FROM qualification_list WHERE id = $1 RETURNING *', [id]);
+    res.send(`Qualification with id ${id} was deleted!`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
 
 module.exports = qualificationsRouter;

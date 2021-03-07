cc791bc8f6921a943a3418f3da385daecae176b1
Becky Dinh <beckyd@uci.edu>
2021-03-07 13:50:43 -0800

    add routes to incomplete qualification

--- routes/accounts/qualifications.js
+++ routes/accounts/qualifications.js
@@ -6,28 +6,61 @@ const pool = require('../../postgres/config');
 
 qualificationsRouter.use(express.json());
 
-// Get all qualifications and their statues for a user, by the user's id
-qualificationsRouter.get('/user/:user_id', async (req, res) => {
-  const { user_id } = req.params;
-  console.log(`Getting qualification statues for userid: ${user_id}`);
+// Get all qualification lists
+qualificationsRouter.get('/', async (req, res) => {
   try {
-    // Join query with qualification and qualification_status tables
+    const allQualificationLists = await pool.query('SELECT * FROM qualification_list');
+    res.send(allQualificationLists.rows);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Get qualification list by id
+qualificationsRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(`Getting qualification_list with id: ${id}`);
+  try {
+    const qualification = await pool.query('SELECT * FROM qualification_list WHERE id = $1', [id]);
+    res.send(qualification.rows);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+qualificationsRouter.get('/:incomplete', async (req, res) => {
+  const { id } = req.params;
+  console.log(`Getting volunteers who need qualifications reviewed ${id}`);
+  try {
+    const qualification = await pool.query('SELECT $1 FROM qualificationStatus WHERE completionStatus=$2', [id, false]);
+    res.send(qualification.rows);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Get qualification list for a user, by their id
+qualificationsRouter.get('/user/:userID', async (req, res) => {
+  const { userID } = req.params;
+  console.log(`Getting qualification_list with userid: ${userID}`);
+  try {
+    // Join query with qualification and qualificationStatus tables
     const userQualificationQuery = await pool.query(`
         SELECT
           qualification.id,
-          qualification.qualification_name,
-          qualification.qualification_description,
-          qualification.volunteer_tier,
-          qualification_status.user_id,
-          qualification_status.completion_status,
-          qualification_status.completion_timestamp,
-          qualification_status.notes
+          qualification.name,
+          qualification.question,
+          qualification.qualificationlistid,
+          qualificationStatus.userID,
+          qualificationStatus.completionStatus,
+          qualificationStatus.completion_timestamp,
+          qualificationStatus.notes
         FROM
-          qualification_status
-        INNER JOIN qualification ON qualification_status.qualification_id = qualification.id
+          qualificationStatus
+        INNER JOIN qualification ON qualificationStatus.qualificationID = qualification.id
         WHERE
-          qualification_status.user_id = $1;
-    `, [user_id]);
+          qualificationStatus.userID = $1;
+    `, [userID]);
 
     res.send(userQualificationQuery.rows);
   } catch (err) {
@@ -35,31 +68,67 @@ qualificationsRouter.get('/user/:user_id', async (req, res) => {
   }
 });
 
-// Get all qualifications
-qualificationsRouter.get('/', async (req, res) => {
+// Create qualification list
+// TODO: Only allow 1 qualification list per volunteer tier
+qualificationsRouter.post('/', async (req, res) => {
   try {
-    const qualifications = await pool.query(`
-        SELECT * FROM qualification
-        ORDER BY
-	        volunteer_tier ASC,
-	        id ASC
-    `);
-    res.send(qualifications.rows);
+    const {
+      volunteerTier,
+    } = req.body;
+    const newQualificationList = await pool.query(`
+        INSERT INTO qualification_list(volunteer_tier, create_timestamp) VALUES
+        ($1,
+        (SELECT NOW()::timestamp)) RETURNING *`,
+    [volunteerTier]);
+    res.send({
+      volunteerTier: newQualificationList.rows,
+    });
   } catch (err) {
     res.status(400).send(err.message);
   }
 });
 
-// Create qualification
-qualificationsRouter.post('/', async (req, res) => {
+// Edit qualification list by id
+qualificationsRouter.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { volunteerTier } = req.body;
+    console.log(`Editing qualification_list with id: ${id}`);
+    if (id == null) res.status(400).send("Can't edit qualification_list without ID");
+    await pool.query(`
+      UPDATE qualification_list
+      SET volunteer_tier = $1
+      WHERE id = $2
+    `,
+    [volunteerTier, id]);
+    res.send(`Qualification with id ${id} was edited!`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Delete qualification list by id
+qualificationsRouter.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (id == null) res.status(400).send("Can't delete qualification_list without ID");
+    await pool.query('DELETE FROM qualification_list WHERE id = $1 RETURNING *', [id]);
+    res.send(`Qualification with id ${id} was deleted!`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Create Qualification
+qualificationsRouter.post('/qualification', async (req, res) => {
   try {
     const {
-      name, description, volunteer_tier
+      name, question, qualificationlistid,
     } = req.body;
     const qualification = await pool.query(`
-        INSERT INTO qualification(qualification_name, qualification_description, volunteer_tier) VALUES
+        INSERT INTO qualification(name, question, qualificationlistid) VALUES
         ($1, $2, $3) RETURNING *`,
-    [name, description, volunteer_tier]);
+    [name, question, qualificationlistid]);
     res.send(
       qualification.rows,
     );
@@ -69,10 +138,10 @@ qualificationsRouter.post('/', async (req, res) => {
 });
 
 // Delete qualification
-qualificationsRouter.delete('/', async (req, res) => {
+qualificationsRouter.delete('/qualification', async (req, res) => {
   try {
     const { id } = req.body;
-    if (id == null) res.status(400).send("Can't delete qualification without ID");
+    if (id == null) res.status(400).send("Can't delete qualification_list without ID");
     await pool.query('DELETE FROM qualification WHERE id = $1 RETURNING *', [id]);
     res.send(`Qualification with id ${id} was deleted!`);
   } catch (err) {
@@ -81,50 +150,47 @@ qualificationsRouter.delete('/', async (req, res) => {
 });
 
 // Edit qualification
-// TODO: Add trigger to update qualification_status table when volunteer_tier changes
-qualificationsRouter.put('/', async(req, res) => {
+qualificationsRouter.put('/qualification', async (req, res) => {
   try {
-    const { id, name, description } = req.body;
+    const { id, name, question } = req.body;
     console.log(id);
     console.log(name);
-    console.log(description);
+    console.log(question);
     if (id == null) res.status(400).send("Can't edit qualification without ID");
-    await pool.query(`
-      UPDATE qualification
-      SET
-        qualification_name = $2,
-        qualification_description = $3
-      WHERE
-        id = $1
-    `,
-    [id, name, description]);
+    const userQuery = `
+UPDATE qualification
+  SET ${name ? `name = '${name}', ` : ''}
+      ${question ? `question = '${question}' ` : ''}
+  WHERE id = ${id}
+    `;
+    await pool.query(userQuery);
     res.send(`Account with id ${id} was updated!`);
   } catch (err) {
     res.status(400).send(err.message);
   }
-})
+});
 
 // Update qualification status
 qualificationsRouter.put('/status', async (req, res) => {
   try {
     const {
-      user_id, qualification_id, completion_status
+      userID, qualificationID, completionStatus,
     } = req.body;
-    console.log(`Setting qualification_status of user: ${user_id} with qualification_id: ${qualification_id} to: ${completion_status}`);
-    if (user_id == null) res.status(400).send("Can't update qualification_status without user_id");
-    else if (qualification_id == null) res.status(400).send("Can't update qualification_status without qualification_id");
-    else if (completion_status == null) res.status(400).send("Can't update qualification_status without completion_status");
+    console.log(`Setting qualificationStatus of user: ${userID} with qualificationID: ${qualificationID} to: ${completionStatus}`);
+    if (userID == null) res.status(400).send("Can't update qualificationStatus without userID");
+    else if (qualificationID == null) res.status(400).send("Can't update qualificationStatus without qualificationID");
+    else if (completionStatus == null) res.status(400).send("Can't update qualificationStatus without completionStatus");
     await pool.query(`
-      UPDATE qualification_status
+      UPDATE qualificationStatus
       SET
-        completion_status = $1
+        completionStatus = $1
         completion_timestamp = SELECT NOW()::timestamp
       WHERE
-        user_id = $2 AND
-        qualification_id = $3
-    `, 
-    [completion_status, user_id, qualification_id]);
-    res.send(`Qualification_status of user ${user_id} with qualification_id ${qualification_id} was updated`);
+        userID = $2 AND
+        qualificationID = $3
+    `,
+    [completionStatus, userID, qualificationID]);
+    res.send(`Qualification_status of user ${userID} with qualificationID ${qualificationID} was updated`);
   } catch (err) {
     res.status(400).send(err.message);
   }

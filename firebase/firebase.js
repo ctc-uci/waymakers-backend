aa3e54b45b47c79585b125e11c3e62007f860f40
Leon Lin <leonlin.48@gmail.com>
2021-04-14 20:35:04 -0700

    hotfix environment variables

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../waymakers-6465d-firebase-adminsdk-2wasv-0fba3e25c9.json');
 
+require('dotenv').config();
+
 let credentials;
 
 if (process.env.NODE_ENV !== 'PRODUCTION') {
@@ -8,12 +10,12 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
 } else {
   credentials = {
     type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
+    project_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.TOKEN_AUTH_URI,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
@@ -24,5 +26,3 @@ admin.initializeApp({
   credential: admin.credential.cert(credentials),
   databaseURL: 'https://waymakers.firebaseio.com',
 });
-
-module.exports = admin;

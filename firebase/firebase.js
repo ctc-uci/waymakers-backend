9fdbe36683518a8033de6a88a018eb4a9a9c6832
Leon Lin <leonlin.48@gmail.com>
2021-04-20 03:16:38 +0000

    Changes for Production Deployment

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,17 +1,14 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../waymakers-6465d-firebase-adminsdk-2wasv-0fba3e25c9.json');
 
 require('dotenv').config();
 
 let credentials;
 
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-  credentials = serviceAccount;
-} else {
+if (process.env.NODE_ENV === 'production') {
   credentials = {
     type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key_id: process.env.FIREBASE_PROJECT_ID,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
@@ -20,6 +17,9 @@ if (process.env.NODE_ENV !== 'PRODUCTION') {
     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
   };
+} else {
+  // eslint-disable-next-line global-require
+  credentials = require('../waymakers-6465d-firebase-adminsdk-2wasv-0fba3e25c9.json');
 }
 
 admin.initializeApp({
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -5,26 +5,28 @@ const admin = require('../../firebase/firebase');
 const authRouter = express();
 
 const verifyToken = async (req, res, next) => {
-  console.log('@verifyToken in');
+  console.log('@verifyToken in', req.cookies);
   const { cookies: { accessToken } } = req;
 
   if (!accessToken) {
-    return res.status(400).send('No access token provided');
+    console.log('No access token provided');
+    return res.status(400).send('@verifyToken No access token provided');
   }
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(accessToken);
     if (!decodedToken) {
-      return res.status(400).send('Empty token from firebase');
+      return res.status(400).send('@verifyToken Empty token from firebase');
     }
-    console.log(decodedToken);
+    console.log('@verifyToken', decodedToken);
 
     return next();
   } catch (err) {
     console.log(err);
-    res.status(400).send('User not logged in');
+    res.status(400).send('@verifyToken User not logged in');
   }
 
+  console.log('Unable to authenticate user');
   return res.status(400).send('Unable to authenticate user');
 };
 
@@ -39,6 +41,7 @@ authRouter.get('/verifyToken/:token', async (req, res) => {
     res.status(200).send(decodedToken.uid);
   } catch (err) {
     // Handle error
+    console.log('@auth/verifyToken', err);
     res.status(400).send(err);
   }
 });
